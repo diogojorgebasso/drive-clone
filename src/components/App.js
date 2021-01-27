d2cf72f62685ef84666507432d5a67e1e8da392a
Diogo Basso <diogojbasso@gmail.com>
2021-01-27 14:10:56 -0300

    update code style

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
-import React from "react"
-import Signup from "./Signup"
-import { Container } from "react-bootstrap"
-import { AuthProvider } from "../contexts/AuthContext"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Dashboard from "./Dashboard"
-import Login from "./Login"
-import PrivateRoute from "./PrivateRoute"
-import ForgotPassword from "./ForgotPassword"
-import UpdateProfile from "./UpdateProfile"
+import React from "react";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Signup from "./auth/Signup";
+import Login from "./auth/Login";
+import PrivateRoute from "./auth/PrivateRoute";
+import ForgotPassword from "./auth/ForgotPassword";
+import UpdateProfile from "./auth/UpdateProfile";
 
-function App() {
+export default function App() {
   return (
     <Container
       className="d-flex align-items-center justify-content-center"
@@ -29,7 +29,5 @@ function App() {
         </Router>
       </div>
     </Container>
-  )
+  );
 }
-
-export default App
