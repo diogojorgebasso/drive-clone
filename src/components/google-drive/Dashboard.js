46edeef94159d756ffae97cf7b51f895e05bc2ad
Diogo Basso <diogojbasso@gmail.com>
2021-01-27 15:57:35 -0300

    add dashboard and fontawsome

--- src/components/google-drive/Dashboard.js
+++ src/components/google-drive/Dashboard.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import AddFolderButton from "./AddFolderButton";
 import Navbar from "./navbar";
+
 export default function Dashboard() {
   return (
-    <Navbar>
-      <Container fluid>Content</Container>
-    </Navbar>
+    <>
+      <Navbar></Navbar>
+
+      <Container fluid>
+        <AddFolderButton />
+      </Container>
+    </>
   );
 }
