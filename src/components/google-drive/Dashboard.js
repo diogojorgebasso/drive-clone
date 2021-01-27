20694f17a458eb01e9315a0a4b36aebfe4294488
Diogo Basso <diogojbasso@gmail.com>
2021-01-27 18:08:01 -0300

    feat: start structuring folders display

--- src/components/google-drive/Dashboard.js
+++ src/components/google-drive/Dashboard.js
@@ -2,16 +2,19 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import AddFolderButton from "./AddFolderButton";
 import Navbar from "./navbar";
+import Folder from "./Folder";
 import { useFolder } from "../../hooks/useFolder";
 
 export default function Dashboard() {
-  const { folder } = useFolder();
+  const { folder } = useFolder("3aenm6sK0sm6HL9dR4p0");
+  console.log(folder);
   return (
     <>
       <Navbar></Navbar>
 
       <Container fluid>
         <AddFolderButton currentFolder={folder} />
+        <Folder folder={folder}></Folder>
       </Container>
     </>
   );
