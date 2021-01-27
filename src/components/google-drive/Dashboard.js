413e8f5f1ff93d57a566e4208097cdadf9a7e05d
Diogo Basso <diogojbasso@gmail.com>
2021-01-27 18:22:43 -0300

    feat: iteration for showing folders

--- src/components/google-drive/Dashboard.js
+++ src/components/google-drive/Dashboard.js
@@ -6,7 +6,7 @@ import Folder from "./Folder";
 import { useFolder } from "../../hooks/useFolder";
 
 export default function Dashboard() {
-  const { folder } = useFolder("3aenm6sK0sm6HL9dR4p0");
+  const { folder, childFolders } = useFolder("3aenm6sK0sm6HL9dR4p0");
   console.log(folder);
   return (
     <>
@@ -14,7 +14,15 @@ export default function Dashboard() {
 
       <Container fluid>
         <AddFolderButton currentFolder={folder} />
-        {folder && <Folder folder={folder}></Folder>}
+        {childFolders.length > 0 && (
+          <div className="d-flex flex-wrap">
+            {childFolders.map((childFolder, index) => (
+              <div key={index} style={{ maxWidth: "250px" }} className="p2">
+                <Folder folder={childFolder} />
+              </div>
+            ))}
+          </div>
+        )}
       </Container>
     </>
   );
