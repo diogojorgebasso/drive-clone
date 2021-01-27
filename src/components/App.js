35294196ba37fe84ad40ebbb85071f55e10e5df6
Diogo Basso <diogojbasso@gmail.com>
2021-01-27 14:36:19 -0300

    fix: auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { AuthProvider } from "../contexts/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Dashboard from "./Dashboard";
+import Profile from "./auth/Profile";
 import Signup from "./auth/Signup";
 import Login from "./auth/Login";
 import PrivateRoute from "./auth/PrivateRoute";
 import ForgotPassword from "./auth/ForgotPassword";
 import UpdateProfile from "./auth/UpdateProfile";
+
 export default function App() {
   return (
     <Router>
       <AuthProvider>
         <Switch>
-          <PrivateRoute exact path="/" component={Dashboard} />
+          <PrivateRoute exact path="/" component={Profile} />
           <PrivateRoute path="/update-profile" component={UpdateProfile} />
           <Route path="/signup" component={Signup} />
           <Route path="/login" component={Login} />
