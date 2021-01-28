7060423c81699712a8302d39632a9a1306897d43
Diogo Basso <diogojbasso@gmail.com>
2021-01-28 19:09:39 -0300

    style: breadcrumb identation

--- src/components/google-drive/FolderBreadcrumbs.js
+++ src/components/google-drive/FolderBreadcrumbs.js
@@ -1,5 +1,19 @@
 import React from "react";
-
-export default function FolderBreadcrumbs() {
-  return <div></div>;
+import { Breadcrumb } from "react-bootstrap";
+export default function FolderBreadcrumbs({ currentFolder }) {
+  return (
+    <Breadcrumb
+      className="flex-grow-1"
+      listProps={{ className: "bg-white pl-0 m-0" }}
+    >
+      {currentFolder && (
+        <Breadcrumb.Item
+          className="text-truncate d-inline-block"
+          style={{ maxWidth: "50px" }}
+        >
+          {currentFolder.name}
+        </Breadcrumb.Item>
+      )}
+    </Breadcrumb>
+  );
 }
