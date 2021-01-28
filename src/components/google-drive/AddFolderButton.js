d83a5d05a92218b2248e6bf23af9a429c3704856
Diogo Basso <diogojbasso@gmail.com>
2021-01-28 19:14:41 -0300

    feat: path for breadcrumbs inside db structure

--- src/components/google-drive/AddFolderButton.js
+++ src/components/google-drive/AddFolderButton.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFolderPlus } from "@fortawesome/free-solid-svg-icons";
 import { database } from "../../firebase";
 import { useAuth } from "../../contexts/AuthContext"; //FIXME: Is it really important to import in this file?
-
+import { ROOT_FOLDER } from "../../hooks/useFolder";
 export default function AddFolderButton({ currentFolder }) {
   const [open, setOpen] = useState(false);
   const [name, setName] = useState("");
@@ -19,12 +19,16 @@ export default function AddFolderButton({ currentFolder }) {
   function handleFolder(event) {
     event.preventDefault();
     if (currentFolder == null) return;
+    const path = [...currentFolder.path];
+    if (currentFolder !== ROOT_FOLDER) {
+      path.push({ name: currentFolder.name, id: currentFolder.id });
+    }
     database.folders.add({
       //Aren't we messing with the separate of concerns?
       name,
       parentId: currentFolder.id,
       userId: currentUser.uid,
-      //path,
+      path,
       timeStamp: database.getTime(),
     });
     setName("");
